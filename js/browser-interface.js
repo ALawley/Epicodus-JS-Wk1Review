b2c3b371f2cd07fe40dfcb503ca25e21ae8f62a5
Anna Lawley <alawley1@gmail.com>
2016-03-18 09:10:17 -0700

    add ui and ui js

---
+++ js/browser-interface.js
@@ -0,0 +1,11 @@
+var getRepos = require('../js/api-call.js').getRepos;
+
+$(document).ready(function() {
+  $('#getrepos').click(function() {
+    var username = $('#username').val();
+    $('#username').val("");
+    var repos = getRepos(username);
+    console.log(repos);
+    $('.showRepos').text("Hello");
+  });
+});
